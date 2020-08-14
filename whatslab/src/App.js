c91df3956c3ada04bd2fb02a00e35294078016ab
Astor Lohmann Palmeira <astorlohmann@gmail.com>
2020-08-14 11:58:59 -0300

    previa exercicio

---
+++ whatslab/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Post'
+import PostWhats from './Componets/Post';
+
+class App extends React.Component {}
+  render(){
+    <PostWhats />,
+  }
+
+export default App;
